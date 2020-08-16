f1aebc689e4ed79ca90e2658fa0a844cf87cda45
0xD <40242723+alxalx14@users.noreply.github.com>
2020-08-17 00:15:10 +0200

    sdf
    asdasd

---
+++ Reviews/reviews.php
@@ -0,0 +1,105 @@
+<?php include "../tawk.php"; ?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Reviews</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+
+<section>
+	<div class="customer-feedback">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-sm-offset-2 col-sm-8">
+					<div>
+						<h2 class="section-title">Client Feedback</h2>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8">
+					<div class="owl-carousel feedback-slider">
+
+
+						<div class="feedback-slider-item">
+							<img src="https://img.etimg.com/thumb/msid-62065348,width-300,resizemode-4/paras-jha-reut.jpg" class="center-block img-circle" alt="Customer Feedback">
+							<h3 class="customer-name">Klokus</h3>
+							<p>I use da portscanner a lot it guud.</p>
+							<span class="light-bg customer-rating" data-rating="5">
+								5
+								<i class="fa fa-star"></i>
+							</span>
+						</div>
+						
+
+						
+						<div class="feedback-slider-item">
+							<img src="https://res.cloudinary.com/hnmqik4yn/image/upload/c_fill,fl_force_strip,h_128,q_100,w_128/v1493982718/ah57hnfnwxkmsciwivve.jpg" class="center-block img-circle" alt="Customer Feedback">
+							<h3 class="customer-name">Cassi</h3>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It is a long established fact that a reader will be distracted by the readable its layout.</p>
+							<span class="light-bg customer-rating" data-rating="4">
+								4
+								<i class="fa fa-star"></i>
+							</span>
+						</div>
+						
+
+						
+						<div class="feedback-slider-item">
+							<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/451270/profile/profile-80.jpg" class="center-block img-circle" alt="Customer Feedback">
+							<h3 class="customer-name">Md Nahidul</h3>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It is a long established fact that a reader will be distracted by the readable its layout.</p>
+							<span class="light-bg customer-rating" data-rating="5">
+								5
+								<i class="fa fa-star"></i>
+							</span>
+						</div>
+						
+						
+					</div>
+
+					
+					<div class="feedback-slider-thumb hidden-xs">
+						<div class="thumb-prev">
+							<span>
+								<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/451270/profile/profile-80.jpg" alt="Customer Feedback">
+							</span>
+							<span class="light-bg customer-rating">
+								5
+								<i class="fa fa-star"></i>
+							</span>
+						</div>
+
+						<div class="thumb-next">
+							<span>
+								<img src="https://res.cloudinary.com/hnmqik4yn/image/upload/c_fill,fl_force_strip,h_128,q_100,w_128/v1493982718/ah57hnfnwxkmsciwivve.jpg" alt="Customer Feedback">
+							</span>
+							<span class="light-bg customer-rating">
+								4
+								<i class="fa fa-star"></i>
+							</span>
+						</div>
+					</div>
+					
+
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
